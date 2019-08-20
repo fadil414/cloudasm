136c8ccf901b95c1fe29d2c2497b58f6c35f4884
fadil414 <52985002+fadil414@users.noreply.github.com>
2019-08-20 12:11:54 +0700

    Update dbcontroller.php

--- dbcontroller.php
+++ dbcontroller.php
@@ -14,7 +14,7 @@ class DBController {
 	}
 	
 	function connectDB() {
-		$conn = mysqli_connect($this->host,$this->user,$this->password,$this->database);
+		$conn = mysqli_connect($this->host,$this->user,$this->password,$this->database,$this->port);
 		return $conn;
 	}
 	
