66d0762c2e91f661a5c6e11aee607f11ca0c0bca
fadil414 <52985002+fadil414@users.noreply.github.com>
2019-08-22 10:55:18 +0700

    Update dbcontroller.php

--- dbcontroller.php
+++ dbcontroller.php
@@ -40,4 +40,20 @@ class DBController {
             return mysqli_error($this->conn);
         }
 }
+$db = parse_url(getenv("DATABASE_URL"));
+		$pdo = new PDO("pgsql:" . sprintf(
+		    "host=%s;port=%s;user=%s;password=%s;dbname=%s",
+		    $db["host"],
+		    $db["port"],
+		    $db["user"],
+		    $db["pass"],
+		    ltrim($db["path"], "/")
+		));
+		
+		$sql = "SELECT studentname, course FROM registercourse";
+		$stmt = $pdo->prepare($sql);
+		//Thiết lập kiểu dữ liệu trả về
+		$stmt->setFetchMode(PDO::FETCH_ASSOC);
+		$stmt->execute();
+		$resultSet = $stmt->fetchAll();
 ?>
